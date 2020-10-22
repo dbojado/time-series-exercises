7906214c4443da1d0a1c2e9667b082c37f3bc670
Daniella Bojado <daniella.bojado@gmail.com>
2020-10-21 21:49:31 -0500

    add

---
+++ acquire.py
@@ -0,0 +1,15 @@
+# Exercise
+
+#1. Using the code from the lesson as a guide, create a dataframe named items that has all of the data for items.
+
+#2. Do the same thing, but for stores.
+
+#3. Extract the data for sales. There are a lot of pages of data here, so your code will need to be a little more complex. Your code should continue fetching data from the next page until all of the data is extracted.
+
+#4. Save the data in your files to local csv files so that it will be faster to access in the future.
+
+#5. Combine the data from your three separate dataframes into one large dataframe.
+
+#6. Acquire the Open Power Systems Data for Germany, which has been rapidly expanding its renewable energy production in recent years. The data set includes country-wide totals of electricity consumption, wind power production, and solar power production for 2006-2017. You can get the data here: https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv
+
+#7. Make sure all the work that you have done above is reproducible. That is, you should put the code above into separate functions in the acquire.py file and be able to re-run the functions and get the same data.#
---
+++ prepare.py
@@ -0,0 +1,15 @@
+# Exercise
+
+#1. Convert date column to datetime format.
+
+#2. Plot the distribution of sale_amount and item_price.
+
+#3. Set the index to be the datetime variable.
+
+#4. Add a 'month' and 'day of week' column to your dataframe, derived from the index using the keywords for those date parts.
+
+#5. Add a column to your dataframe, sales_total, which is a derived from sale_amount (total items) and item_price.
+
+#6. Using pandas.DataFrame.diff() function, create a new column that is the result of the current sales - the previous days sales.
+
+#7. Make sure all the work that you have done above is reproducible. That is, you should put the code above into separate functions and be able to re-run the functions and get the same results.
